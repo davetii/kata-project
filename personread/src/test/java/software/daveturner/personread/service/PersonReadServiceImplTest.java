8d1efcaa19038e7c0e9c36269803e711be09592b
davetii <davetii@gmail.com>
2024-08-11 22:08:39 -0400

    Cleaning up code

--- personread/src/test/java/software/daveturner/personread/service/PersonReadServiceImplTest.java
+++ personread/src/test/java/software/daveturner/personread/service/PersonReadServiceImplTest.java
@@ -2,7 +2,6 @@ package software.daveturner.personread.service;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -12,6 +11,8 @@ import software.daveturner.personread.repo.PersonReadRepo;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.doReturn;
 
 @SpringBootTest
 class PersonReadServiceImplTest {
@@ -28,7 +29,7 @@ class PersonReadServiceImplTest {
         person.setId("123");
 
         Optional<Person> mockedData = Optional.of(person);
-        Mockito.doReturn(mockedData).when(repo).findById(Mockito.anyString());
+        doReturn(mockedData).when(repo).findById(anyString());
 
         Optional<Person> value = service.findById("123");
         assertTrue(value.isPresent());
